13dd7cd0222c8aad5e961a049f6c3ae29041dfd5
JamesFowler42 <keith@difficult.me.uk>
2013-12-24 15:12:53 +0000

    Version 1.4.0
    Improvements to the graphing function

--- website/morpheuz/view014.js
+++ website/morpheuz/view014.js
@@ -28,15 +28,15 @@
 Date.prototype.format = function(format) //author: meizz
 {
 	var o = {
-			"M+" : this.getMonth() + 1, //month
-			"d+" : this.getDate(), //day
-			"h+" : this.getHours(), //hour
-			"i+" : this.getHours() + 1, //hour + 1
-			"m+" : this.getMinutes(), //minute
-			"s+" : this.getSeconds(), //second
-			"q+" : Math.floor((this.getMonth() + 3) / 3), //quarter
-			"S" : this.getMilliseconds()
-			//millisecond
+		"M+" : this.getMonth() + 1, //month
+		"d+" : this.getDate(), //day
+		"h+" : this.getHours(), //hour
+		"i+" : this.getHours() + 1, //hour + 1
+		"m+" : this.getMinutes(), //minute
+		"s+" : this.getSeconds(), //second
+		"q+" : Math.floor((this.getMonth() + 3) / 3), //quarter
+		"S" : this.getMilliseconds()
+	//millisecond
 	}
 
 	if (/(y+)/.test(format))
@@ -44,9 +44,8 @@ Date.prototype.format = function(format) //author: meizz
 				.substr(4 - RegExp.$1.length));
 	for ( var k in o)
 		if (new RegExp("(" + k + ")").test(format))
-			format = format.replace(RegExp.$1,
-					RegExp.$1.length == 1 ? o[k] : ("00" + o[k])
-							.substr(("" + o[k]).length));
+			format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k]
+					: ("00" + o[k]).substr(("" + o[k]).length));
 	return format;
 }
 
@@ -59,7 +58,7 @@ Date.prototype.addMinutes = function(minutes) {
  * Set a dropdown list to the value
  */
 function setSelectedValue(selectObj, valueToSet) {
-	selectObj.style.display='inline';
+	selectObj.style.display = 'inline';
 	for (var i = 0; i < selectObj.options.length; i++) {
 		if (selectObj.options[i].text == valueToSet) {
 			selectObj.options[i].selected = true;
@@ -72,7 +71,7 @@ function setSelectedValue(selectObj, valueToSet) {
  * Set a textbox to the value
  */
 function setTextValue(selectObj, valueToSet) {
-	selectObj.style.display='inline';
+	selectObj.style.display = 'inline';
 	selectObj.value = fixLen(valueToSet);
 }
 
@@ -88,8 +87,9 @@ function getSelectedValue(selectObj) {
  */
 function getTextValue(selectObj, hour) {
 	var value = parseInt(selectObj.value, 10);
-	if (isNaN(value) || (hour && (value < 0 || value > 23)) || (!hour && (value < 0 || value > 59))) {
-		selectObj.style.backgroundColor='red';
+	if (isNaN(value) || (hour && (value < 0 || value > 23))
+			|| (!hour && (value < 0 || value > 59))) {
+		selectObj.style.backgroundColor = 'red';
 		return "X";
 	}
 	return fixLen(String(value));
@@ -110,13 +110,15 @@ function fixLen(inStr) {
 function getParameterByName(name) {
 	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
 	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex
-	.exec(location.search);
-	return results == null ? "" : decodeURIComponent(results[1]
-	.replace(/\+/g, " "));
+			.exec(location.search);
+	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g,
+			" "));
 }
 
 // Spot if we are on iOS or not 
-document.ios = navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i);
+document.ios = navigator.userAgent.match(/iPhone/i)
+		|| navigator.userAgent.match(/iPad/i)
+		|| navigator.userAgent.match(/iPod/i);
 
 // Pick up parameters from URL
 var baseStr = getParameterByName('base');
@@ -139,8 +141,8 @@ var smartOn = smart == 'Y';
 document.getElementById('smartalarm').checked = smartOn;
 
 if (document.ios) {
-        document.getElementById('reset').className='reset';
-        document.getElementById('copy').className='copy';
+	document.getElementById('reset').className = 'reset';
+	document.getElementById('copy').className = 'copy';
 	setSelectedValue(document.getElementById('safromhour'), fromhr);
 	setSelectedValue(document.getElementById('safrommin'), frommin);
 	setSelectedValue(document.getElementById('satohour'), tohr);
@@ -160,13 +162,13 @@ var startPoint = new Date(base);
 var splitup = graph.split("!");
 var more = new Array();
 
-for (var i=0; i<splitup.length; i++) {
+for (var i = 0; i < splitup.length; i++) {
 	if (splitup[i] == '')
 		continue;
 	var element = new Array();
 	element[0] = startPoint;
 	element[1] = parseInt(splitup[i], 10);
-	if (element[1] == -1) 
+	if (element[1] == -1)
 		element[1] = null;
 	more[i] = element;
 	startPoint = startPoint.addMinutes(10);
@@ -180,8 +182,8 @@ if (smartOn) {
 	var smartStartPoint = new Date(base);
 	var early = null;
 	var late = null;
-       var actual = null;
-	for (var i=0; i<splitup.length; i++) {
+	var actual = null;
+	for (var i = 0; i < splitup.length; i++) {
 		var teststr1 = smartStartPoint.format("hhmm");
 		var smartStartPoint1 = smartStartPoint;
 		smartStartPoint = smartStartPoint.addMinutes(10);
@@ -190,127 +192,199 @@ if (smartOn) {
 			early = smartStartPoint1;
 		if (late == null && tostr >= teststr1 && tostr <= teststr2)
 			late = smartStartPoint1;
-		if (actual == null && goneoff != 'N' && goneoff >= teststr1 && goneoff <= teststr2)
+		if (actual == null && goneoff != 'N' && goneoff >= teststr1
+				&& goneoff <= teststr2)
 			actual = smartStartPoint1;
-		if (late != null && early != null && actual != null) 
+		if (late != null && early != null && actual != null)
 			break;
 	}
 	if (early != null && late != null) {
-               var canvasOverlayActual;
+		var canvasOverlayActual;
 		if (actual != null) {
-		   canvasOverlayActual =  {verticalLine: {
-				        	                              name: "actual",
-				        	                              x: actual,
- 				        	                              lineWidth: 1,
-				        	                              yOffset: 0,
-				        	                              color: 'rgb(255, 149, 0)',
-				        	                              shadow: false
-				                                             }};
+			canvasOverlayActual = {
+				verticalLine : {
+					name : "actual",
+					x : actual,
+					lineWidth : 1,
+					yOffset : 0,
+					color : 'rgb(255, 149, 0)',
+					shadow : false
+				}
+			};
 		}
 		canvasOverlayConf = {
-				show: true,
-				objects: [
-				          {dashedVerticalLine: {
-				        	  name: "start",
-				        	  x: early,
-				        	  lineWidth: 1,
-				        	  yOffset: 0,
-				        	  dashPattern: [1, 4],
-				        	  color: 'rgb(76, 217, 100)',
-				        	  shadow: false
-				          }},
-				          {dashedVerticalLine: {
-				        	  name: "end",
-				        	  x: late,
-				        	  lineWidth: 1,
-				        	  yOffset: 0,
-				        	  dashPattern: [1, 4],
-				        	  color: 'rgb(255, 59, 48)',
-				        	  shadow: false
-				          }},
-					  canvasOverlayActual
-				          ]
+			show : true,
+			objects : [ {
+				dashedVerticalLine : {
+					name : "start",
+					x : early,
+					lineWidth : 1,
+					yOffset : 0,
+					dashPattern : [ 1, 4 ],
+					color : 'rgb(76, 217, 100)',
+					shadow : false
+				}
+			}, {
+				dashedVerticalLine : {
+					name : "end",
+					x : late,
+					lineWidth : 1,
+					yOffset : 0,
+					dashPattern : [ 1, 4 ],
+					color : 'rgb(255, 59, 48)',
+					shadow : false
+				}
+			}, canvasOverlayActual ]
 		};
 	}
 }
 
-// Prepare the graph
-$(document).ready(function() {
-	var plot2 = $.jqplot('chart1', [more], {
-		grid: {
-			background: '#ffffff'
-		},
-		animate: true,
-		canvasOverlay : canvasOverlayConf,
-		series : [ {
-			showMarker : false,
-			breakOnNull : true,
-			color: 'rgb(0,122,255)',
-			label: new Date(base).format('yyyy-MM-dd'),
-			trendline: {
-				show: true,       
-				color: 'rgb(90,200,250)',   
-				label: '', 
-				type: 'linear', 
-				shadow: true, 
-				lineWidth: 1.5, 
-				shadowAngle: 45, 
-				shadowOffset: 1.5,  
-				shadowDepth: 3,  
-				shadowAlpha: 0.07
-			}
-		} ],
-		// You can specify options for all axes on the plot at once with
-		// the axesDefaults object.  Here, we're using a canvas renderer
-		// to draw the axis label which allows rotated text.
-		axesDefaults : {
-			labelRenderer : $.jqplot.CanvasAxisLabelRenderer,
-			showTicks : false,
-			showTickMarks : false,
-		},
-		legend: {
-			show: true,
-			location: 'ne'
-		},
-		// An axes object holds options for all axes.
-		// Allowable axes are xaxis, x2axis, yaxis, y2axis, y3axis, ...
-		// Up to 9 y axes are supported.
-		axes : {
-			// options for each axis are specified in seperate option objects.
-			xaxis : {
-				renderer:$.jqplot.DateAxisRenderer, 
-				tickRenderer: $.jqplot.CanvasAxisTickRenderer ,
-				tickOptions:{formatString:'%R', angle: -30, fontSize: '8pt'},
-				tickInterval:'1 hour',
-				// Turn off "padding".  This will allow data point to lie on the
-				// edges of the grid.  Default padding is 1.2 and will keep all
-				// points inside the bounds of the grid.
-				pad : 0,
-				showTicks : true,
-				showTickMarks : true,
-				min: new Date(base)
-			},
-			yaxis : {
-				label : "Movement",
-				min : -50,
-				max : 4000,
-			}
+// Get the full set of data up to the wake up point. 
+// Ignore nulls
+var set2 = new Array();
+var pieStartPoint = new Date(base);
+var points = 0;
+var total = 0;
+for (var i = 0; i < splitup.length; i++) {
+	if (splitup[i] == '')
+		continue;
+	var data = parseInt(splitup[i], 10);
+	var teststr1 = pieStartPoint.format("hhmm");
+	var pieStartPoint1 = pieStartPoint;
+	pieStartPoint = pieStartPoint.addMinutes(10);
+	var teststr2 = pieStartPoint.format("hhmm");
+	if (goneoff != 'N' && goneoff >= teststr1 && goneoff <= teststr2)
+		break;
+	if (data == -1)
+		continue;
+	set2[points] = data;
+	points++;
+	total = total + data;
+}
+var awake = 0;
+var deep = 0;
+var light = 0;
+if (points != 0) {
+	var average = total / points;
+	for (var i = 0; i < points; i++) {
+		if (set2[i] > 1000) {
+			awake++;
+		} else if (set2[i] > average) {
+			light++;
+		} else {
+			deep++;
 		}
-	});
-});
+	}
+}
+
+var data2 = [ [ 'Awake', awake ], [ 'Light', light ], [ 'Deep', deep ] ];
+
+// Prepare the graph
+$(document).ready(
+		function() {
+
+			var plot1 = $.jqplot('chart1', [ more ], {
+				grid : {
+					background : '#ffffff'
+				},
+				animate : true,
+				canvasOverlay : canvasOverlayConf,
+				series : [ {
+					showMarker : false,
+					breakOnNull : true,
+					color : 'rgb(0,122,255)',
+					label : new Date(base).format('yyyy-MM-dd'),
+					trendline : {
+						show : true,
+						color : 'rgb(90,200,250)',
+						label : '',
+						type : 'linear',
+						shadow : true,
+						lineWidth : 1.5,
+						shadowAngle : 45,
+						shadowOffset : 1.5,
+						shadowDepth : 3,
+						shadowAlpha : 0.07
+					}
+				} ],
+				// You can specify options for all axes on the plot at once with
+				// the axesDefaults object.  Here, we're using a canvas renderer
+				// to draw the axis label which allows rotated text.
+				axesDefaults : {
+					labelRenderer : $.jqplot.CanvasAxisLabelRenderer,
+					showTicks : false,
+					showTickMarks : false,
+				},
+				legend : {
+					show : true,
+					location : 'ne'
+				},
+				// An axes object holds options for all axes.
+				// Allowable axes are xaxis, x2axis, yaxis, y2axis, y3axis, ...
+				// Up to 9 y axes are supported.
+				axes : {
+					// options for each axis are specified in seperate option objects.
+					xaxis : {
+						renderer : $.jqplot.DateAxisRenderer,
+						tickRenderer : $.jqplot.CanvasAxisTickRenderer,
+						tickOptions : {
+							formatString : '%R',
+							angle : -30,
+							fontSize : '8pt'
+						},
+						tickInterval : '1 hour',
+						// Turn off "padding".  This will allow data point to lie on the
+						// edges of the grid.  Default padding is 1.2 and will keep all
+						// points inside the bounds of the grid.
+						pad : 0,
+						showTicks : true,
+						showTickMarks : true,
+						min : new Date(base)
+					},
+					yaxis : {
+						label : "Movement",
+						min : -50,
+						max : 4000,
+					}
+				}
+			});
+
+			var plot2 = jQuery.jqplot('chart2', [ data2 ], {
+				grid : {
+					background : '#ffffff'
+				},
+				seriesColors : [ "rgb(90, 200, 250)", "rgb(0,122,255)",
+						"rgb(88,86,214)" ],
+				seriesDefaults : {
+					// Make this a pie chart.
+					renderer : jQuery.jqplot.PieRenderer,
+					rendererOptions : {
+						// Put data labels on the pie slices.
+						// By default, labels show the percentage of the slice.
+						showDataLabels : true
+					}
+				},
+				legend : {
+					show : true,
+					location : 'e'
+				}
+			});
+
+		});
 
 // Prepare the copy link
 var timePoint = new Date(base);
 var body = '&body=';
 
-for (var i=0; i<splitup.length; i++) {
+for (var i = 0; i < splitup.length; i++) {
 	if (splitup[i] == '')
 		continue;
 	body = body + timePoint.format('hh:mm') + ',' + splitup[i] + '%0D%0A';
 	timePoint = timePoint.addMinutes(10);
 }
 
-var mailto = 'mailto:?subject=Morpheuz-' + new Date(base).format('yyyy-MM-dd') + '.csv' + body;
+var mailto = 'mailto:?subject=Morpheuz-' + new Date(base).format('yyyy-MM-dd')
+		+ '.csv' + body;
 
 document.getElementById('copy').href = mailto;
 
@@ -327,57 +401,29 @@ document.getElementById('reset').onclick = function() {
 		tohrpart = getSelectedValue(document.getElementById('satohour'));
 		tominpart = getSelectedValue(document.getElementById('satomin'));
 	} else {
-		fromhrpart = getTextValue(document.getElementById('safromhourtxt'), true);
-		fromminpart = getTextValue(document.getElementById('safrommintxt'), false);
+		fromhrpart = getTextValue(document.getElementById('safromhourtxt'),
+				true);
+		fromminpart = getTextValue(document.getElementById('safrommintxt'),
+				false);
 		tohrpart = getTextValue(document.getElementById('satohourtxt'), true);
-		tominpart = getTextValue(document.getElementById('satomintxt'),  false);
-		if (fromhrpart == "X" || fromminpart == "X" || tohrpart == "X" || tominpart == "X")
+		tominpart = getTextValue(document.getElementById('satomintxt'), false);
+		if (fromhrpart == "X" || fromminpart == "X" || tohrpart == "X"
+				|| tominpart == "X")
 			return;
 	}
 
-	if  ((fromhrpart + ":" + fromminpart) >= (tohrpart + ":" + tominpart)) {
-		document.getElementById('safromhour').style.backgroundColor='red';
-		document.getElementById('safrommin').style.backgroundColor='red';
-		document.getElementById('satohour').style.backgroundColor='red';
-		document.getElementById('satomin').style.backgroundColor='red';
-		document.getElementById('safromhourtxt').style.backgroundColor='red';
-		document.getElementById('safrommintxt').style.backgroundColor='red';
-		document.getElementById('satohourtxt').style.backgroundColor='red';
-		document.getElementById('satomintxt').style.backgroundColor='red';
+	if ((fromhrpart + ":" + fromminpart) >= (tohrpart + ":" + tominpart)) {
+		document.getElementById('safromhour').style.backgroundColor = 'red';
+		document.getElementById('safrommin').style.backgroundColor = 'red';
+		document.getElementById('satohour').style.backgroundColor = 'red';
+		document.getElementById('satomin').style.backgroundColor = 'red';
+		document.getElementById('safromhourtxt').style.backgroundColor = 'red';
+		document.getElementById('safrommintxt').style.backgroundColor = 'red';
+		document.getElementById('satohourtxt').style.backgroundColor = 'red';
+		document.getElementById('satomintxt').style.backgroundColor = 'red';
 	} else {
-		window.location.href = "pebblejs://close#reset" + '!' + smartpart
-		+ '!' + fromhrpart + '!' + fromminpart + '!' + tohrpart
-		+ '!' + tominpart;
+		window.location.href = "pebblejs://close#reset" + '!' + smartpart + '!'
+				+ fromhrpart + '!' + fromminpart + '!' + tohrpart + '!'
+				+ tominpart;
 	}
-}
-
-if (!document.ios) {
-	document.getElementById('diag1').style.display='inline';
-	document.getElementById('diag2').style.display='inline';
-}
-
-// Diag button handling
-document.getElementById('diag1').onclick = function() {
-	var	fromhrpart = getTextValue(document.getElementById('safromhourtxt'), true);
-	var	fromminpart = getTextValue(document.getElementById('safrommintxt'), false);
-	var	tohrpart = getTextValue(document.getElementById('satohourtxt'), true);
-	var	tominpart = getTextValue(document.getElementById('satomintxt'),  false);
-
-	var booleanEval = (fromhrpart + ":" + fromminpart) >= (tohrpart + ":" + tominpart); 
-
-	window.alert("from=[" + (fromhrpart + ":" + fromminpart) + "], to=[" + (tohrpart + ":" + tominpart) + "], booleanEval=" + booleanEval);
-}
-document.getElementById('diag2').onclick = function() {
-
-	var selectObj = document.getElementById('safromhourtxt');
-	
-	var textValue = selectObj.value;
-
-	var value = parseInt(textValue, 10);
-
-	var valueStr = String(value);
-
-	var fixStr = fixLen(valueStr);
-
-	window.alert("textValue=[" + textValue + "], value=[" + value + "], valueStr=[" + valueStr + "], fixStr=[" + fixStr + "]"); 
 }
